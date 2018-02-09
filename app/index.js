273e79abca404779adc2b8790764be8e27719ece
Quentin Roy <quentin@quentinroy.fr>
2018-02-09 12:36:12 +0100

    fix: navigation history not being saved

--- app/index.js
+++ app/index.js
@@ -2,28 +2,36 @@ import store from "store";
 import polygonWidget from "./polygon-widget";
 import "./index.scss";
 
+const STORE_VERTEX_KEY = "polygon-vertexes";
+
 const { getVertexes, setVertexes } = polygonWidget({
-  initVertexes: store.get("polygon-vertexes"),
+  // Init the vertexes using the local storage.
+  initVertexes: store.get(STORE_VERTEX_KEY),
+  // Handle vertexes change.
   onChanged: type => {
     if (type === "set") return;
     const vertexes = getVertexes();
-    store.set("polygon-vertexes", vertexes);
+    // Set up local storage (for refresh and next page load).
+    store.set(STORE_VERTEX_KEY, vertexes);
+    // Set up history.
     if (type !== "drag") {
       window.history.pushState({ vertexes }, null, null);
     }
   }
 });
 
-document.querySelector(".clear").addEventListener("click", () => {
-  setVertexes([]);
-  store.remove("polygon-vertexes");
-  window.history.pushState(null, null, null);
-});
-
+// Handle history navigation.
 window.addEventListener("popstate", e => {
-  setVertexes((e.state && e.state.vertexes) || []);
+  const vertexes = e.state && e.state.vertexes;
+  setVertexes(vertexes || []);
+  if (!vertexes) {
+    store.remove(STORE_VERTEX_KEY);
+  } else {
+    store.set(STORE_VERTEX_KEY, vertexes);
+  }
 });
 
+// Map undo/redo keyboard shortcuts to history.
 window.addEventListener("keydown", e => {
   if (e.key === "z") {
     if (
@@ -42,4 +50,10 @@ window.addEventListener("keydown", e => {
       e.preventDefault();
     }
   }
+});
+
+document.querySelector(".clear").addEventListener("click", () => {
+  setVertexes([]);
+  store.remove(STORE_VERTEX_KEY);
+  window.history.pushState(null, null, null);
 });
